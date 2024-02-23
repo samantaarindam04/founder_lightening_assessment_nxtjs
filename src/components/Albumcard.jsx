2764c66c6d4c363e2ce012e71279b452519f8f7c
samantaarindam04 <samantaarindam04@gmail.com>
2024-02-23 17:19:36 +0530

    Update Albumcard.jsx

--- src/components/Albumcard.jsx
+++ src/components/Albumcard.jsx
@@ -35,7 +35,7 @@ export default function Albumcard({ albums, username }) {
     <ErrorBoundaries>
       {loading ? <h3 style={{ gridColumnStart: 3 }}>Loading...</h3> :
         albums && albums.map((key, index) =>
-          <div className={styles.single_album} onClick={handlePhoto} key={index} data-id={key.id} data-user={key.userId} data-name={key.title} data-username={key.username}>
+          <div className={styles.single_album} onClick={handlePhoto} key={index} data-id={key.id} data-user={key.userId} data-name={key.title} data-username={key.username} data-testid={`single_album_test_${index}`}>
             <Image src={`/folder.png`} priority={true} width={200} height={200} alt={`folder_img`} blurDataURL={`data:image/gif;base64,R0lGODlhAQABAPAAAO21BgEA/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`} />
             <span>{key.title}</span>
           </div>
